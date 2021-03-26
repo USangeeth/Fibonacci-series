d1f00814ee7bff7e2ac0d0394b62c5bbce88773e
USangeeth <81401858+USangeeth@users.noreply.github.com>
2021-03-26 09:53:34 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,27 @@
+# printing positive numbers in the given range
+# list of numbers
+list1 = [12,-7,5,64,-14]
+num = 0
+  
+# using while loop     
+while(num < len(list1)):
+      
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+      
+    # increment num 
+    num += 1
+    
+list2 = [12,14,-95,3]
+num2 = 0
+  
+# using while loop     
+while(num2 < len(list1)):
+      
+    # checking condition
+    if list1[num2] >= 0:
+        print(list1[num2], end = " ")
+      
+    # increment num 
+    num2 += 1
